583910b3de1d03caf57f2b8f21e3e7d65269f189
mikolaj <mikolajt55@gmail.com>
2025-06-25 15:04:13 +0200

    Implement set method, add shader loading check

--- src/ShaderManager.cpp
+++ src/ShaderManager.cpp
@@ -2,11 +2,23 @@
 
 void Engine::ShaderManager::init() {
     defaultShader = LoadShader(0, 0);
-    solidShader = LoadShader("../shaders/solid.vs", "../solid.fs");
-    materialPreviewShader = LoadShader("../shaders/material_preview.vs", "../shaders/material_preview.fs");
+    solidShader = LoadShader("../shaders/solid.vs", "../shaders/solid.fs");
+    materialPreviewShader = LoadShader("../shaders/raylibshaders/lighting.vs", "../shaders/raylibshaders/lighting.fs");
+
+    if (!IsShaderValid(materialPreviewShader) || !IsShaderValid(defaultShader) || IsShaderValid(solidShader)) {
+        throw std::runtime_error("ShaderManager::Engine::ShaderManager::init: Failed to load shader!");
+    }
 }
-Shader Engine::ShaderManager::set() {
-    return defaultShader;
+Shader Engine::ShaderManager::set(const Shader& prev) {
+    if (IsKeyPressed(KEY_F1)) {
+        return solidShader;
+    } else if (IsKeyPressed(KEY_F2)) {
+        return defaultShader;
+    } else if (IsKeyPressed(KEY_F3)) {
+        return materialPreviewShader;
+    }
+
+    return prev;
 }
 void Engine::ShaderManager::clean() {
     UnloadShader(defaultShader);
