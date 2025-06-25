2ad5a9898ca6328a6ad21e13c1c665017b2890f5
mikolaj <mikolajt55@gmail.com>
2025-06-25 20:21:30 +0200

    Add manager class to make managing states way easier

---
+++ src/StateManager.cpp
@@ -0,0 +1,38 @@
+#include "StateManager.hpp"
+
+Engine::StateManager::StateManager() :
+m_add(false),
+m_remove(false),
+m_replace(false) {
+
+}
+void Engine::StateManager::add(std::unique_ptr<Engine::bStateTemplate> toAdd, bool replace) {
+    m_add = true;
+    newState = std::move(toAdd);
+
+    m_replace = replace;
+}
+void Engine::StateManager::popCurrent() {
+    m_remove = true;
+}
+void Engine::StateManager::processState() {
+
+    if (m_remove && (!stateStack.empty())) {
+        stateStack.pop();
+        m_remove = false;
+    }
+
+    if (m_add) {
+        if (m_replace) {
+            stateStack.pop();
+            m_replace = false;
+        }
+
+        stateStack.push(std::move(newState));
+        stateStack.top()->init();
+        m_add = false;
+    }
+}
+std::unique_ptr<Engine::bStateTemplate> &Engine::StateManager::getCurrentState() {
+    return stateStack.top();
+}
---
+++ src/StateManager.hpp
@@ -0,0 +1,30 @@
+#ifndef STATEMANAGER_HPP
+#define STATEMANAGER_HPP
+
+#include <stack>
+#include <memory>
+#include "bStateTemplate.hpp"
+
+namespace Engine {
+
+class StateManager {
+private:
+    std::stack<std::unique_ptr<bStateTemplate>> stateStack;
+    std::unique_ptr<bStateTemplate> newState;
+
+    bool m_add;
+    bool m_replace;
+    bool m_remove;
+public:
+    StateManager();
+    ~StateManager() = default;
+
+    void add(std::unique_ptr<bStateTemplate> state, bool replace = false);
+    void popCurrent();
+    void processState();
+    std::unique_ptr<bStateTemplate>& getCurrentState();
+};
+
+}
+
+#endif //STATEMANAGER_HPP
