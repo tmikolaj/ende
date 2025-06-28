755e53d4cf5375fcecfeaca2ac7b4ebe89c294c7
mikolaj <mikolajt55@gmail.com>
2025-06-28 19:04:40 +0200

    Add std::unique_ptr to std::vector<Entity> in Context

--- src/Program.hpp
+++ src/Program.hpp
@@ -1,20 +1,21 @@
 #ifndef PROGRAM_HPP
 #define PROGRAM_HPP
 
-#include <list>
 #include "bStateTemplate.hpp"
 #include "bEngineComponent.hpp"
 #include "ShaderManager.hpp"
 #include "StateManager.hpp"
 #include "MainMenu.hpp"
+#include "Entity.hpp"
 
 struct Context {
     // TODO:
     std::unique_ptr<Engine::StateManager> states;
-    std::unique_ptr<std::list<Model*>> models;
+    std::unique_ptr<std::vector<Entity>> entities;
 
     Context() {
         states = std::make_unique<Engine::StateManager>();
+        entities = std::make_unique<std::vector<Entity>>();
     }
 };
 
