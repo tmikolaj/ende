dc804c2ff9471d22ea528c50b1cb1a2e5cef32a3
mikolaj <mikolajt55@gmail.com>
2025-07-07 13:28:17 +0200

    Add random header, seedValue and helper methods (getSeedValue, updateSeedValue, genNewSeedValue)

--- src/Noise.cpp
+++ src/Noise.cpp
@@ -1,7 +1,39 @@
+#include <cstring>
 #include "Noise.hpp"
 
+Noise::Noise() :
+dist(-10000, 10000) {
+
+}
+
+void Noise::init(char _seedBuffer[25]) {
+    if (_seedBuffer[0] != '\0') {
+        std::strncpy(seedBuffer, _seedBuffer, sizeof(seedBuffer));
+        gen.seed(std::stoi(seedBuffer));
+    } else {
+        gen.seed(rd());
+    }
+    seedValue = dist(gen);
+}
+
+int Noise::getSeedValue() {
+    return seedValue;
+}
+
+void Noise::updateSeedValue(int newVal) {
+    seedValue = newVal;
+}
+
+int Noise::genNewSeedValue() {
+    seedValue = dist(gen);
+    return seedValue;
+}
+
 float Noise::getBasicPerlin(float x, float z) {
-    return (sinf(x * frequency) * cosf(z * frequency)) * amplitude;
+    float offsetx =  x + seedValue;
+    float offsetz =  z + seedValue;
+
+    return (sinf(offsetx * frequency) * cosf(offsetz * frequency)) * amplitude;
 }
 
 float Noise::getFakePerlin(float x, float z) {
@@ -17,7 +49,7 @@ float Noise::getOctave(float x, float z) {
         float nx = x * freq;
         float ny = z * freq;
 
-        float noiseVal = getFakePerlin(nx, ny);
+        float noiseVal = getFakePerlin(nx + seedValue, ny + seedValue);
         total += noiseVal * amp;
 
         amp *= persistence;
--- src/Noise.hpp
+++ src/Noise.hpp
@@ -2,8 +2,14 @@
 #define BASICPERLIN_HPP
 
 #include <cmath>
+#include <random>
 
 class Noise {
+private:
+    char seedBuffer[25];
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_real_distribution<float> dist;
 public:
     float frequency;
     float amplitude;
@@ -12,9 +18,15 @@ public:
     float persistence;
     float lacunarity;
 
-    Noise() = default;
+    int seedValue;
+
+    Noise();
     ~Noise() = default;
 
+    void init(char _seedBuffer[25]);
+    int getSeedValue();
+    void updateSeedValue(int newVal);
+    int genNewSeedValue();
     float getBasicPerlin(float x, float z);
     float getFakePerlin(float x, float z);
     float getOctave(float x, float z);
