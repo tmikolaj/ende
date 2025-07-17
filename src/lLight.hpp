d9eb30c588a346f909967babe7fb80982ffb8fb7
mikolaj <mikolajt55@gmail.com>
2025-07-17 13:44:50 +0200

    Add lLight struct

---
+++ src/lLight.hpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "rlights.h"
+
+struct lLight {
+    Light _l_light;
+    bool deleted;
+    std::string name;
+
+    lLight(const std::string& _name, Light l) : _l_light(l), deleted(false), name(_name) {}
+};
