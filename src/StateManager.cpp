ec6ad3eeb5beafa1e80aed425f38d04ad6c75812
mikolaj <mikolajt55@gmail.com>
2025-07-21 13:43:20 +0200

    Implement setContext and requestStateChange functions, refactor to pass the context into state functions

--- src/StateManager.cpp
+++ src/StateManager.cpp
@@ -1,4 +1,6 @@
 #include "StateManager.hpp"
+#include "States/Scene.hpp"
+#include "States/StartMenu.hpp"
 
 Engine::StateManager::StateManager() :
 m_add(false),
@@ -7,37 +9,54 @@ m_replace(false),
 windowStatus(0) {
 
 }
-void Engine::StateManager::add(std::unique_ptr<Engine::bStateTemplate> toAdd, bool replace) {
+
+void Engine::StateManager::setContext(std::weak_ptr<Context> ctx) {
+    context = ctx;
+}
+
+void Engine::StateManager::add(std::unique_ptr<bStateTemplate> toAdd, bool replace) {
     m_add = true;
     newState = std::move(toAdd);
 
     m_replace = replace;
 }
+
 void Engine::StateManager::popCurrent() {
     m_remove = true;
 }
+
 void Engine::StateManager::processState() {
+    auto ctx = context.lock();
 
     if (m_remove && (!stateStack.empty())) {
-        getCurrentState()->clean();
+        getCurrentState()->clean(ctx);
         stateStack.pop();
         m_remove = false;
     }
 
     if (m_add) {
         if (m_replace) {
-            stateStack.top()->clean();
+            stateStack.top()->clean(ctx);
             stateStack.pop();
             m_replace = false;
         }
 
         stateStack.push(std::move(newState));
-        stateStack.top()->init();
+        stateStack.top()->init(ctx);
         m_add = false;
     }
 }
-std::unique_ptr<Engine::bStateTemplate> &Engine::StateManager::getCurrentState() {
+
+std::unique_ptr<bStateTemplate> &Engine::StateManager::getCurrentState() {
     return stateStack.top();
+}
+
+void Engine::StateManager::requestStateChange(int stateIndex, bool replace) {
+    if (stateIndex == STARTMENU) newState = std::move(std::make_unique<StartMenu>());
+    else if (stateIndex == SCENE) newState = std::move(std::make_unique<Scene>());
+
+    m_replace = replace;
+    m_add = true;
 }
 
 void Engine::StateManager::setWindowState(int status) {
