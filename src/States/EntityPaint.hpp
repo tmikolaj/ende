f0df7558278445cca9225a74e01313f50100ec3a
mikolaj <mikolajt55@gmail.com>
2025-07-22 14:55:26 +0200

    Add brush, render, draw variables and two function (ImVec4ToColor and CheckCollisionRayTriangle)

--- src/States/EntityPaint.hpp
+++ src/States/EntityPaint.hpp
@@ -5,7 +5,20 @@
 
 class EntityPaint : public bStateTemplate {
 private:
+
+    // brush related variables
+    float brushSize;
+    ImVec4 paintColor;
+    RenderTexture2D paintCanvas;
+    int textureWidth;
+    int textureHeight;
+    bool validHit;
+    Vector3 hitPos;
+    Vector3 hitNormal;
+
+    // render/draw variables
     int currentShader;
+    int selectedEntity;
 public:
     EntityPaint() = default;
     ~EntityPaint() override = default;
@@ -14,6 +27,9 @@ public:
     void process(std::shared_ptr<Context> &m_context) override;
     void draw(std::shared_ptr<Context> &m_context) override;
     void clean(std::shared_ptr<Context> &m_context) override;
+
+    Color ImVec4ToColor(ImVec4 p_color);
+    bool CheckCollisionRayTriangle(const Ray& ray, Vector3 v0, Vector3 v1, Vector3 v2, Vector3* p_hitPos);
 };
 
 #endif //ENTITYPAINT_HPP
