4c39b481f6335ff8c363479b522afcb572c88095
mikolaj <mikolajt55@gmail.com>
2025-07-22 13:39:13 +0200

    Move camera init to Program (from Scene state), add ImGui::DestroyContext on exit

--- src/Program.cpp
+++ src/Program.cpp
@@ -6,12 +6,22 @@ context(std::make_shared<Context>()) {
 }
 void Program::init() {
     InitWindow(800, 600, "3DProcGen");
+
     context->states->setContext(context);
     context->states->setWindowState(NONE);
+
     rlImGuiSetup(true);
+
     context->fontMgr.init();
+
     context->states->add(std::make_unique<StartMenu>());
     context->states->processState();
+
+    context->camera->position = { 5, 0, 5 };
+    context->camera->target = { 0, 0, 0 };
+    context->camera->up = { 0, 1, 0 };
+    context->camera->fovy = 45.0f;
+    context->camera->projection = CAMERA_PERSPECTIVE;
 }
 void Program::run() {
     int w_state = context->states->getWindowState();
@@ -20,6 +30,7 @@ void Program::run() {
             w_state = context->states->getWindowState();
             if (w_state == EXIT) {
                 context->states->getCurrentState()->clean(context);
+                ImGui::DestroyContext();
                 exit(0);
             }
             context->states->processState();
