9cf22d89705b46c79539c29004847b7c0a067b75
mikolaj <mikolajt55@gmail.com>
2025-07-23 14:03:47 +0200

    Add texcoords recalculation

--- src/shapers/SubdivisionShaper.cpp
+++ src/shapers/SubdivisionShaper.cpp
@@ -33,13 +33,20 @@ int SubdivisionShaper::GetMidpoint(int i1, int i2, std::vector<float>& vertices,
 }
 
 void SubdivisionShaper::Apply(std::unique_ptr<Entity>& e) {
+    Texture2D tex = e->e_model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture;
+
     subdivisions++;
 
     std::vector<float> newVertices = e->e_vertices;
     std::vector<unsigned short> newIndices;
     std::vector<float> newNormals;
 
+    std::vector<float> newTexcoords;
+
     std::map<std::pair<int, int>, int> midpointCache;
+
+    float minX = FLT_MAX, maxX = -FLT_MAX;
+    float minZ = FLT_MAX, maxZ = -FLT_MAX;
 
     for (int i = 0; i < e->e_mesh->triangleCount; i++) {
         int i0 = e->e_indices[i * 3];
@@ -56,6 +63,27 @@ void SubdivisionShaper::Apply(std::unique_ptr<Entity>& e) {
         newIndices.push_back(a); newIndices.push_back(b); newIndices.push_back(c);
     }
 
+    for (int i = 0; i < newVertices.size(); i += 3) {
+        float x = newVertices[i];
+        float z = newVertices[i + 2];
+
+        if (x < minX) minX = x;
+        if (x > maxX) maxX = x;
+        if (z < minZ) minZ = z;
+        if (z > maxZ) maxZ = z;
+    }
+
+    for (int i = 0; i < newVertices.size(); i++) {
+        float x = newVertices[i * 3];
+        float z = newVertices[i * 3 + 2];
+
+        float u = (x - minX) / (maxX - minX);
+        float v = (z - minZ) / (maxZ - minZ);
+
+        newTexcoords.push_back(u);
+        newTexcoords.push_back(v);
+    }
+
     e->RecalcNormals();
     e->e_normals = newNormals;
 
@@ -69,8 +97,11 @@ void SubdivisionShaper::Apply(std::unique_ptr<Entity>& e) {
     newMesh.indices = static_cast<unsigned short*>(RL_MALLOC(sizeof(unsigned short) * newIndices.size()));
     memcpy(newMesh.indices, newIndices.data(), sizeof(unsigned short) * newIndices.size());
 
-    newMesh.normals  = (float*)RL_MALLOC(sizeof(float) * newNormals.size());
+    newMesh.normals = static_cast<float*>(RL_MALLOC(sizeof(float) * newNormals.size()));
     memcpy(newMesh.normals, newNormals.data(), sizeof(float) * newNormals.size());
+
+    newMesh.texcoords = static_cast<float*>(RL_MALLOC(sizeof(float) * newTexcoords.size()));
+    memcpy(newMesh.texcoords, newTexcoords.data(), sizeof(float) * newTexcoords.size());
 
     newMesh.vertexCount = static_cast<int>(newVertices.size()) / 3;
     newMesh.triangleCount = static_cast<int>(newIndices.size()) / 3;
@@ -82,4 +113,6 @@ void SubdivisionShaper::Apply(std::unique_ptr<Entity>& e) {
     e->e_mesh = new Mesh(newMesh);
 
     e->e_model = LoadModelFromMesh(newMesh);
+
+    e->e_model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = tex;
 }
