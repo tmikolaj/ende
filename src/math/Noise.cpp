ebcb9d4abf27738f19d7783f1e7d4c19d19049fe
mikolaj <mikolajt55@gmail.com>
2025-07-24 12:05:12 +0200

    Add getPerlinNoiseTerrain and getSinglePerlinNoiseTerrain (helper) functions and terracing

--- src/math/Noise.cpp
+++ src/math/Noise.cpp
@@ -1,9 +1,13 @@
 #include <cstring>
 #include "Noise.hpp"
 
+#define TWO_PI (std::numbers::pi * 2.0f)
+
 Noise::Noise() :
 dist(-10000, 10000),
-improvedFakeNoise(false) {
+improvedFakeNoise(false),
+enableTerracing(false),
+terraceSteps(1.0f) {
 
 }
 
@@ -45,9 +49,9 @@ float Noise::fade(float t) {
 }
 
 float Noise::hash(int x, int z, bool useSeed) {
-    const int n = x * 374761 + z * 668265;
+    const int n = x * 374761 + z * 668265 + (useSeed ? seedValue : 0);
 
-    float result = sinf(n * 0.0001f) * 43758.5453f;
+    const float result = sinf(static_cast<float>(n) * 0.0001f) * 43758.5453f;
 
     return (result - floorf(result)) * 2.0f - 1.0f;
 }
@@ -141,6 +145,72 @@ float Noise::getValueNoiseTerrain(float x, float z, bool useSeed, bool makeFract
     }
 
     return total;
+}
+
+float Noise::getPerlinNoiseTerrain(float x, float z, bool useSeed, bool makeFractal) {
+    float total = 0.0f;
+    float freq = frequency;
+    float amp = amplitude;
+
+    const int iter = makeFractal ? octaves : 1;
+
+    for (int i = 0; i < iter; i++) {
+
+        float value = getSinglePerlinNoiseTerrain(x, z, freq, useSeed);
+
+        value *= amp;
+        total += value;
+
+        freq *= lacunarity;
+        amp *= persistence;
+    }
+    if (enableTerracing) total = floorf(total * terraceSteps) / terraceSteps;
+
+    return total;
+}
+
+float Noise::getSinglePerlinNoiseTerrain(float x, float z, float freq, bool useSeed) {
+    float sx = x * freq;
+    float sz = z * freq;
+
+    const int x0 = static_cast<int>(floorf(sx));
+    const int z0 = static_cast<int>(floorf(sz));
+
+    const int x1 = x0 + 1;
+    const int z1 = z0 + 1;
+
+    const float dx = sx - static_cast<float>(x0);
+    const float dz = sz - static_cast<float>(z0);
+
+    // vectors
+    const float angleA = hash(x0, z0, useSeed) * TWO_PI;
+    glm::vec2 gradientA(cosf(angleA), sinf(angleA));
+    const float angleB = hash(x1, z0, useSeed) * TWO_PI;
+    glm::vec2 gradientB(cosf(angleB), sinf(angleB));
+    const float angleC = hash(x0, z1, useSeed) * TWO_PI;
+    glm::vec2 gradientC(cosf(angleC), sinf(angleC));
+    const float angleD = hash(x1, z1, useSeed) * TWO_PI;
+    glm::vec2 gradientD(cosf(angleD), sinf(angleD));
+
+    // offsets
+    const glm::vec2 offA(dx, dz);
+    const glm::vec2 offB(dx - 1.0f, dz);
+    const glm::vec2 offC(dx, dz - 1.0f);
+    const glm::vec2 offD(dx - 1.0f, dz - 1.0f);
+
+    const float dotA = glm::dot(gradientA,  offA);
+    const float dotB = glm::dot(gradientB, offB);
+    const float dotC = glm::dot(gradientC, offC);
+    const float dotD = glm::dot(gradientD, offD);
+
+    const float u = fade(dx);
+    const float v = fade(dz);
+
+    // interpolation
+    const float ix0 = lerp(dotA, dotB, u);
+    const float ix1 = lerp(dotC, dotD, u);
+
+    return lerp(ix0, ix1, v);
 }
 
 // -------------
--- src/math/Noise.hpp
+++ src/math/Noise.hpp
@@ -21,10 +21,12 @@ public:
     int octaves;
     float persistence;
     float lacunarity;
+    float terraceSteps;
 
     int seedValue;
 
     bool improvedFakeNoise;
+    bool enableTerracing;
 
     Noise();
     ~Noise() = default;
@@ -36,9 +38,11 @@ public:
 
     // TERRAIN SPECIFIC NOISE
     float getSimplePatternTerrain(float x, float z, bool useSeed);
-    float getFakePerlinTerrain(float x, float z);
+    float getFakePerlinTerrain(float x, float z); // HELPER for getFractalNoiseTerrain!
     float getFractalNoiseTerrain(float x, float z, bool useSeed);
     float getValueNoiseTerrain(float x, float z, bool useSeed, bool makeFractal);
+    float getPerlinNoiseTerrain(float x, float z, bool useSeed, bool makeFractal);
+    float getSinglePerlinNoiseTerrain(float x, float z, float freq, bool useSeed); // HELPER for getPerlinNoiseTerrain
 
     // ROCK SPECIFIC NOISE
     glm::vec3 getSimplePatternRock(const glm::vec3& v, bool useSeed);
